b58d255c00da8639834b601b7e958ac1f38cdc89
allentey2 <24aekren@athenian.org>
2020-10-29 10:44:51 -0700

    day 2 notes

--- my_code.py
+++ my_code.py
@@ -2,9 +2,9 @@
 # Collaborators:               (fill this is on every assignment, even if the answer is "none")
 '''
 george
-'''
 
-print ('hello world!')
+
+print('hello world!')
 
 name = 'Andrew'
 
@@ -12,8 +12,44 @@ print(f'hello {name}')
 
 name = 'TheLordGeorge'
 
-print (f'hello {name}')
+print(f'hello {name}')
 
 name2 = 'Andrew'
 
-print (f'hello {name} and {name2}.')
+print(f'hello {name} and {name2}.')
+
+# honestly I don't know
+
+print(type(name))
+
+#instead of f strings
+
+print('hello '+name+'?')
+
+height_tree_stuff = 5
+heightTreeStuff = 6
+
+true = True
+false = False
+
+# f strings are noice
+
+print(f'the height_tree_stuff is {height_tree_stuff} feet tall')
+'''
+
+#time for da user input
+#print('why do u exsist?')
+#life = input()
+#print(f'I like {life} too')
+
+#food = input("what is your favorite snack? ")
+#print(f'I like {food} too')
+
+
+print ('enter a number:')
+num = int(input())
+#num = int(num)
+double = num + 2
+print(double)
+
+
